8552616d9c0b459f4fab7689e133082b389643d2
Shirel <99799701+shirelzz@users.noreply.github.com>
2024-03-06 18:17:24 +0200

    compiled yolov5 model

--- SafeDrive/Detector.swift
+++ SafeDrive/Detector.swift
@@ -1,80 +1,129 @@
+////
+////  Detector.swift
+////  SafeDrive
+////
+////  Created by שיראל זכריה on 05/03/2024.
+////
 //
-//  Detector.swift
-//  SafeDrive
+//import Foundation
+//import Vision
+//import AVFoundation
+//import UIKit
 //
-//  Created by שיראל זכריה on 05/03/2024.
+//extension ViewController {
+//    
 //
-
-import Foundation
-import Vision
-import AVFoundation
-import UIKit
-
-extension ViewController {
-    
-    func setupDetector() {
-        let modelURL = Bundle.main.url(forResource: "yolov5s", withExtension: "mlmodelc")
-    
-        do {
-            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL!))
-            let recognitions = VNCoreMLRequest(model: visionModel, completionHandler: detectionDidComplete)
-            self.requests = [recognitions]
-        } catch let error {
-            print(error)
-        }
-    }
-    
-    func detectionDidComplete(request: VNRequest, error: Error?) {
-        DispatchQueue.main.async(execute: {
-            if let results = request.results {
-                self.extractDetections(results)
-            }
-        })
-    }
-    
-    func extractDetections(_ results: [VNObservation]) {
-        detectionLayer.sublayers = nil
-        
-        for observation in results where observation is VNRecognizedObjectObservation {
-            guard let objectObservation = observation as? VNRecognizedObjectObservation else { continue }
-            
-            // Transformations
-            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(screenRect.size.width), Int(screenRect.size.height))
-            let transformedBounds = CGRect(x: objectBounds.minX, y: screenRect.size.height - objectBounds.maxY, width: objectBounds.maxX - objectBounds.minX, height: objectBounds.maxY - objectBounds.minY)
-            
-            let boxLayer = self.drawBoundingBox(transformedBounds)
-
-            detectionLayer.addSublayer(boxLayer)
-        }
-    }
-    
-    func setupLayers() {
-        detectionLayer = CALayer()
-        detectionLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
-        self.view.layer.addSublayer(detectionLayer)
-    }
-    
-    func updateLayers() {
-        detectionLayer?.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
-    }
-    
-    func drawBoundingBox(_ bounds: CGRect) -> CALayer {
-        let boxLayer = CALayer()
-        boxLayer.frame = bounds
-        boxLayer.borderWidth = 3.0
-        boxLayer.borderColor = CGColor.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-        boxLayer.cornerRadius = 4
-        return boxLayer
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:]) // Create handler to perform request on the buffer
-
-        do {
-            try imageRequestHandler.perform(self.requests) // Schedules vision requests to be performed
-        } catch {
-            print(error)
-        }
-    }
-}
+//    func setupDetector() {
+//        guard let modelURL = Bundle.main.url(forResource: "yolov5s", withExtension: "mlmodelc", subdirectory: nil) else {
+//            fatalError("Model not found")
+//        }
+//
+//        do {
+//            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
+//            print("Model: \(visionModel.description)")
+//
+////            let recognitionRequest = VNCoreMLRequest(model: visionModel, completionHandler: { [weak self] request, error in
+////                self?.detectionDidComplete(request: request, error: error)
+////            })
+//            let recognitionRequest = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
+//
+//                DispatchQueue.main.async(execute: {
+//                    // perform all the UI updates on the main queue
+//                    if let results = request.results {
+//                        print("results 0: \(results)")
+//                        self.extractDetections(results)
+//                        
+//                        if results.isEmpty {
+//                            print("results is empty")
+//                        }
+//                    }
+//                    print("no results")
+//
+//                })
+//            })
+//            
+//            print("recognitionRequest model: \(recognitionRequest.model)")
+//            print("recognitionRequest model: \(recognitionRequest.debugDescription)")
+//            
+//            requests = [recognitionRequest]
+//            
+//
+//            
+//        } catch {
+//            print("Error loading Core ML model: \(error)")
+//        }
+//    }
+//
+//    func detectionDidComplete(request: VNRequest, error: Error?) {
+//        guard let results = request.results else {
+//            print("No results found")
+//            return
+//        }
+//        print("Results received: \(results)")
+//        
+//        if results.isEmpty {
+//            print("results is empty")
+//        }
+//
+//
+//        DispatchQueue.main.async {
+//            self.extractDetections(results)
+//            print("Object detection results: \(results)")
+//        }
+//    }
+//
+//    
+//    func extractDetections(_ results: [Any]) {
+//        print("results 3: \(results)")
+//        detectionLayer.sublayers?.forEach { $0.removeFromSuperlayer() }
+//        
+//        for observation in results where observation is VNRecognizedObjectObservation {
+//            guard let objectObservation = observation as? VNRecognizedObjectObservation else { continue }
+//            
+//            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox,
+//                                                            Int(screenRect.size.width),
+//                                                            Int(screenRect.size.height))
+//            print("Object Bounds: \(objectBounds)")
+//
+//            let transformedBounds = CGRect(
+//                x: objectBounds.minX,
+//                y: screenRect.size.height - objectBounds.maxY,
+//                width: objectBounds.maxX - objectBounds.minX,
+//                height: objectBounds.maxY - objectBounds.minY)
+//            print("transformedBounds: \(transformedBounds)")
+//            
+//            let boxLayer = drawBoundingBox(transformedBounds)
+//            detectionLayer.addSublayer(boxLayer)
+//            
+//            print("Drawing bounding box at: \(boxLayer.frame)")
+//
+//        }
+//    }
+//    
+//    func setupLayers() {
+//        detectionLayer = CALayer()
+//        detectionLayer.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+////        detectionLayer.zPosition = 1
+////        detectionLayer.backgroundColor = UIColor.blue.withAlphaComponent(1).cgColor
+//
+//        self.view.layer.addSublayer(detectionLayer)
+//        print("Detection layer frame: \(detectionLayer.frame)")
+//        print("finished setting layers")
+//    }
+//
+//    func updateLayers() {
+//        detectionLayer?.frame = CGRect(x: 0, y: 0, width: screenRect.size.width, height: screenRect.size.height)
+//    }
+//    
+//    func drawBoundingBox(_ bounds: CGRect) -> CALayer {
+//        let boxLayer = CALayer()
+//        boxLayer.frame = bounds
+//        boxLayer.borderWidth = 3.0
+//        boxLayer.borderColor = UIColor.red.cgColor
+//        boxLayer.cornerRadius = 4
+//        print("Drawing bounding box at: \(bounds)")
+//
+//        return boxLayer
+//    }
+//    
+//}
